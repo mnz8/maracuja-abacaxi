31485efe12e731c1c97b0df8a40977923b75c98e
line126 <line112266@126.com>
2023-05-20 13:48:38 +0800

    feat: restructure

--- svelte.config.js
+++ svelte.config.js
@@ -11,8 +11,20 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+        adapter: adapter(),
+        files: {
+            appTemplate: 'src-svelte/app.html',
+            routes: 'src-svelte/routes',
+            hooks: {
+                client: 'src-svelte/hooks.client',
+                server: 'src-svelte/hooks.server',
+            },
+            lib: 'src-svelte/lib',
+            params: 'src-svelte/params',
+            serviceWorker: 'src-svelte/service-worker',
+            errorTemplate: 'src-svelte/error.html',
+        },
+    },
 };
 
 export default config;
